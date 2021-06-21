7942fce203649cf131cf1a3d1c92f1afd1ba1446
seric1237 <80081733+seric1237@users.noreply.github.com>
2021-06-22 01:20:36 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,26 +10,20 @@ input.onButtonPressed(Button.A, function () {
     if (player1 == player2) {
         basic.showString("Draw")
     }
-    for (let index = 0; index < 3; index++) {
-        basic.showNumber(P1_score)
-        basic.pause(1000)
-    }
-    for (let index = 0; index < 3; index++) {
-        basic.showNumber(P2_score)
-        basic.pause(1000)
-    }
+    basic.showNumber(P1_score)
+    basic.clearScreen()
+    basic.pause(1000)
+    basic.showNumber(P2_score)
+    basic.clearScreen()
+    basic.pause(1000)
     player1 = 0
     player2 = 0
 })
 input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 3; index++) {
-        basic.showNumber(P1_score)
-        basic.pause(1000)
-    }
-    for (let index = 0; index < 3; index++) {
-        basic.showNumber(P2_score)
-        basic.pause(1000)
-    }
+    basic.showNumber(P1_score)
+    basic.pause(1000)
+    basic.showNumber(P2_score)
+    basic.pause(1000)
 })
 input.onButtonPressed(Button.B, function () {
     radio.setGroup(2)
@@ -38,13 +32,24 @@ input.onButtonPressed(Button.B, function () {
     player1 = 0
     player2 = 0
     basic.showString("reset")
+    basic.clearScreen()
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "P1") {
         player1 = value
+        if (value >= 10) {
+            basic.showString("white")
+        } else {
+            basic.showString("black")
+        }
     }
     if (name == "P2") {
         player2 = value
+        if (value >= 10) {
+            basic.showString("white")
+        } else {
+            basic.showString("black")
+        }
     }
 })
 let player2 = 0
